eb4a185e73842b1de7de6edcb35ac26ad6c7cd92
unknown <3j36matsuo@gmail.com>
2019-11-05 10:01:16 +0900

    関数を追加

--- servo.py
+++ servo.py
@@ -1,24 +1,24 @@
 import time
 import RPi.GPIO as GPIO
 
+setservo(range):
+	if -90<=range<=90:
+		duty=range*(2.4-0.5)/20*100
+		servo.ChangeDutyCycle(duty)
+
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(2, GPIO.OUT)
 servo = GPIO.PWM(2, 50)
 servo.start(0.0)
 
-bottom = 2.5
-middle = 7.2
-top = 12.0
 
 for i in range(5):
-	servo.ChangeDutyCycle(bottom)
-	time.sleep(1.0)
+	setservo(-90)
 
-	servo.ChangeDutyCycle(middle)
-	time.sleep(1.0)
+	setservo(0)
 
-	servo.ChangeDutyCycle(top)
-	time.sleep(1.0)
+	setservo(90)
 
 
 
